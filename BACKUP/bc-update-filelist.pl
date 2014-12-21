bb884f4c15240f5eeb5437601ebfa5d3a6abe4dc
Barry Carter <carter.barry@gmail.com>
2014-12-21 07:15:48 -0700

    Checkpoint

---
+++ BACKUP/bc-update-filelist.pl
@@ -0,0 +1,25 @@
+#!/bin/perl
+
+# Instead of creating "find / -ls" daily, this program attempts to
+# find (roughly) the creation time of the previous file list and only
+# find files newer than it, and then merge [or perhaps version-ate?]
+
+require "/usr/local/lib/bclib.pl";
+
+# NOTE: considered using existing file list and finding most recent
+# file (and then backing up a day to be safe), but that seems
+# pointless now
+
+# TODO: other file dumps
+# TODO: decide if I want to keep these files bzip'd or not
+# TODO: do this for reverse file search thing too
+
+$file = "/usr/local/etc/kevin59/files/bcunix-files.txt";
+
+my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+$atime,$mtime,$ctime,$blksize,$blocks) = stat($file);
+
+# the minimum of file times minus a day, in "touch" format
+my($touch) = strftime("%Y%m%d%H%M", gmtime(min($mtime,$ctime,$atime)-86400));
+
+system("touch -t $touch /tmp/buf.timestamp");
